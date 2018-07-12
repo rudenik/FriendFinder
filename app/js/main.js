2703ad6528ac8884cb6676815d11101307c0b98f
Simon Cain <rudenik@gmail.com>
2018-07-12 11:16:57 -0400

    starting flusing out collecting form data.

---
+++ app/js/main.js
@@ -0,0 +1,20 @@
+function surveyPost(){
+    var currUrl = window.location.origin;
+
+    // $.ajax({ url: currUrl + "survey", method: GET}).then(
+
+    // )
+    var newFriend = {
+        name: $("#namefield").val().trim(),
+        //photo?
+        score: [$("#q1").val(), $("#q2").val(), $("#q3").val(), $("#q4").val(), $("#q5").val()]
+    }
+    console.log("from: " + currUrl);
+    console.log("Form Name : " + newFriend.name);
+    console.log("FormData: " + newFriend.score[0] + " " + newFriend.score[1] + " " + newFriend.score[2]+ " " + newFriend.score[3]+ " " + newFriend.score[4]);
+}
+
+$("#subbutt").on("click", function(e){
+    e.preventDefault();
+    surveyPost();
+})
